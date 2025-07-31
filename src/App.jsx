8c499a4d6250d39df68e18da91794d016330820a
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-31 08:23:35 -0400

    Multi Routes was implimented

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react'
-import './App.css'
-import AuthPage from './pages/AuthPage'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
-import DashboardLayout from './pages/DashboardLayout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import DashboardLayout from './pages/DashboardLayout';
+import Dashboard from './components/Dashboard';
+
+import './App.css';
+import sidebarItems from './data/sidebarItems';
 
 function App() {
-
   return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<AuthPage />} />
-            <Route path='/home' element={<DashboardLayout /> } />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/home" element={<DashboardLayout />}>
+
+          <Route index element={<Dashboard />} />
+          {sidebarItems
+            .filter(section => section.basePath && section.items.some(item => item.path && item.element))
+            .map((section, index) =>
+              section.items.map((item, subIndex) => (
+                <Route
+                  key={`${index}-${subIndex}`}
+                  path={`${section.basePath}/${item.path}`}
+                  element={item.element}
+                />
+              ))
+            )}
+          <Route path="*" element={<div className="p-6">404 - Page Not Found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
