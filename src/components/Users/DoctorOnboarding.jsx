e6960e3141d101f39ac5ac053160b0540d8d9b47
bhogesh4i9 <vurabhogesh@gmail.com>
2025-08-01 05:55:24 -0400

    Imported the Componet of DocTable here to display the details of the doc details.

--- src/components/Users/DoctorOnboarding.jsx
+++ src/components/Users/DoctorOnboarding.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import DoctorTable from '../UserComponents/DoctorTable';
 
-const DoctorOnboarding = () => {
+const DoctorOnboarding =  () => {
   return (
-    <div>DoctorOnboarding</div>
-  )
-}
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Doctor Onboarding Approval</h1>
+      <DoctorTable />
+    </div>
+  );
+};
 
 export default DoctorOnboarding;
