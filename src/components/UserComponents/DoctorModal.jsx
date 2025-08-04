de5f5c6fbe67a8421daec4f733dd4857bafe6611
bhogesh4i9 <vurabhogesh@gmail.com>
2025-08-04 02:56:20 -0400

    Make the title at center and also add cursor-pointer to the buttons.

--- src/components/UserComponents/DoctorModal.jsx
+++ src/components/UserComponents/DoctorModal.jsx
@@ -6,11 +6,11 @@ const DoctorModal = ({ isOpen, onClose, title, content }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-xl shadow-xl w-[90%] max-w-md">
-        <h2 className="text-xl font-semibold mb-4">{title}</h2>
+        <h2 className="text-center ext-xl font-semibold mb-4">{title}</h2>
         <div className="text-sm">{content}</div>
         <div className="flex justify-end mt-4">
           <button
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+            className="cursor-pointer px-4 py-2 bg-black text-white rounded "
             onClick={onClose}
           >
             Close
