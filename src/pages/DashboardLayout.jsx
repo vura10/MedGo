f11fdc5ac91570fb4dadf9556e33dc9f53947851
bhogesh4i9 <vurabhogesh@gmail.com>
2025-08-12 05:32:35 -0400

    Replace the place of Hamburger from Sidebar to Navbar.

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,4 +1,3 @@
-// src/pages/DashboardLayout.js
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
@@ -19,37 +18,25 @@ const DashboardLayout = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <Navbar />
-      <div className="flex flex-1 w-full">
-        {/* Sidebar */}
-        {isSidebarOpen && (
-          <div className="z-30 fixed lg:static w-60 lg:w-auto">
-            <Sidebar onClose={() => setIsSidebarOpen(false)} />
-          </div>
-        )}
+    <div className="min-h-screen bg-gray-100 flex flex-col w-full max-w-[100vw] overflow-x-hidden box-border">
+  <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
 
-        {/* Toggle Button - shown only when sidebar is closed and screen < lg */}
-        {!isSidebarOpen && (
-          <button
-            onClick={() => setIsSidebarOpen(true)}
-            className="cursor-pointer absolute top-15 left-3 z-40 bg-white p-2 rounded shadow lg:hidden"
-          >
-            <FaBars size={15} />
-          </button>
-        )}
-
-        {/* Main Content */}
-        <main
-          className={clsx(
-            "flex-1 p-6 transition-all duration-300 w-full",
-            isSidebarOpen && "lg:ml-10"
-          )}
-        >
-          <Outlet />
-        </main>
+  <div className="flex flex-1">
+    {isSidebarOpen && (
+      <div className="fixed lg:static top-0 left-0 z-40">
+        <Sidebar onClose={() => setIsSidebarOpen(false)} />
       </div>
-    </div>
+    )}
+    <main
+      className={clsx(
+        "flex-1 p-4 sm:p-6 md:p-1 transition-all duration-300",
+        isSidebarOpen ? "lg:ml-2" : "ml-0"
+      )}
+    >
+      <Outlet />
+    </main>
+  </div>
+</div>
   );
 };
 
