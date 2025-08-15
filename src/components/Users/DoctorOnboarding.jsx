fb962321e2bb7b5f2c81a642bc488dbf94aa4c8d
bhogesh4i9 <vurabhogesh@gmail.com>
2025-08-15 02:55:42 -0400

    Heading size was convert fro h1 - h2

--- src/components/Users/DoctorOnboarding.jsx
+++ src/components/Users/DoctorOnboarding.jsx
@@ -4,7 +4,7 @@ import DoctorTable from './UserComponents/DoctorTable';
 const DoctorOnboarding =  () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Doctor Onboarding Approval</h1>
+      <h2 className="text-1xl font-bold mb-4">Doctor Onboarding Approval</h2>
       <DoctorTable />
     </div>
   );
